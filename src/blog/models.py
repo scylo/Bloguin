ab0ffb69cc7d4bb6dc17930f26535a18f53e824b
W3nd <wendellholden@hotmail.com>
2024-01-09 23:46:24 -0300

    models, views, urls and migrations. +admin

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'base.html', locals())
+
+def post_list(request):
+    posts = Post.objects.order_by('-pub_date')
+    return render(request, 'post_list.html', locals())
