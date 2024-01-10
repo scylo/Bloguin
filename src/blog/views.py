64505d00fae3f1a736426b5c7a7d3bf77f486319
W3nd <wendellholden@hotmail.com>
2024-01-10 01:00:16 -0300

    button to add post

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,5 +1,11 @@
+'''
+dependendo da organização do projeto, as views devem ficar em arquivos separados
+neste projeto, por simplicidade, as views ficam dentro do mesmo arquivo
+'''
+
 from django.shortcuts import get_object_or_404, render
 from .models import Post
+from .forms import PostForm
 
 
 def index(request):
@@ -14,3 +20,22 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     comments = post.comments.all()
     return render(request, 'post_detail.html', locals())
+
+
+def add_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            # dessa forma, salva autor de acordo com o escolhido no form
+            post = form.save()
+            
+            # ou salva o usuário que está criando o post
+
+            # cria o post sem salvar pois vamos fazer isso depois
+            # post = form.save(commit=False)
+            # post.author = request.user
+            # post.save()
+            return render(request, 'post_detail.html', locals())
+    else:
+        form = PostForm()
+    return render(request, 'add_post.html', locals())
