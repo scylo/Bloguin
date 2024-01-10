0959a3e5356d08c6fbfbbf52abd7ac7482bdf513
W3nd <wendellholden@hotmail.com>
2024-01-10 00:36:17 -0300

    post details(urls, view and htmls)

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("post_list", views.post_list, name="post_list"),
+    path("post_detail/<int:pk>", views.post_detail, name="post_detail"),
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 from .models import Post
 
 
@@ -9,3 +8,9 @@ def index(request):
 def post_list(request):
     posts = Post.objects.order_by('-pub_date')
     return render(request, 'post_list.html', locals())
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    comments = post.comments.all()
+    return render(request, 'post_detail.html', locals())
